369657bbb713994307b939366de062d05f4f517c
PrHiGo <thomas_palmqvist@live.se>
2023-04-20 08:42:30 +0200

    Base structure of the files is done

---
+++ ASP.NETSubmission/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NETSubmission.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
